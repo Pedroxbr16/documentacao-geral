f15d1d56cba981166482f5f86818135391ad1c78
pedro <pjusto@cge.rj.gov.br>
2024-10-07 13:41:52 -0300

    botao certo

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
         <Link
             className="button button--secondary button--lg"
-            to="/docs/wsl.md">
+            to="docs/wsl">
             Base de Conhecimento
           </Link>
         </div>
